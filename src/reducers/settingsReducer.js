c26a32e3bf1711b6f5117dd46becfbbf70078357
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-05 15:26:40 +0500

    Update settingsReducer.js

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,14 +1,17 @@
-
-const updateSettings = (settingName, settingValue, state) => {
-  const updatedSettings = { ...state, [settingName]: settingValue };
-  return updatedSettings;
-};
-
-// eslint-disable-next-line import/prefer-default-export
 export const settingsReducer = (state, action) => {
   switch (action.type) {
-    case 'UPDATE_SETTINGS':
-      return updateSettings(action.settingName, action.settingValue, state);
+    case 'UPDATE_SETTINGS': {
+      const { settingName, settingValue } = action;
+
+      // optional guard (prevents accidental undefined keys)
+      if (!settingName) return state;
+
+      return {
+        ...state,
+        [settingName]: settingValue,
+      };
+    }
+
     default:
       return state;
   }
